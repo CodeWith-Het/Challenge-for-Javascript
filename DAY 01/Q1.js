ba68f89724e7ed1ecdfa04b42b5fe7a222224047
CodeWith-Het <hetp1065@gmail.com>
2025-08-23 08:46:27 -0400

    Added1

---
+++ DAY 01/Q1.js
@@ -0,0 +1,6 @@
+// Write a Program to reverse a string in JavaScript.
+const reversestring = (str) =>{
+    return str.split("").reverse().join("");
+    
+}
+console.log(reversestring("Het Patel"))
